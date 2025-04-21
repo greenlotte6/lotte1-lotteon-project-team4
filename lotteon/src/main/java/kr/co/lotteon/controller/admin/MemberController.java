c493376d625a9bd6f3c285ec1a7fa112d0100751
gyeol000726 <gyeol000726@gmail.com>
2025-04-21 22:12:27 +0900

    add all

--- lotteon/src/main/java/kr/co/lotteon/controller/admin/MemberController.java
+++ lotteon/src/main/java/kr/co/lotteon/controller/admin/MemberController.java
@@ -16,26 +16,15 @@ public class MemberController {
 
     private final UsersService usersService;
 
-//    @GetMapping("/admin/member/list")
-//    public String list(String uid, Model model) {
-//
-//        UsersDTO usersDTO = usersService.findAll(uid);
-//        model.addAttribute("users", users);
-//
-//        // 회원 목록 불러오기
-//        return "users";
-//    }
+    @GetMapping("/admin/member/list")
+    public String list(Model model) {
 
-//    @GetMapping("/article/view")
-//    public String view(int no, Model model) {
-//
-//        // 글 조회 서비스 호출
-//        ArticleDTO articleDTO = articleService.findById(no);
-//
-//        model.addAttribute(articleDTO);
-//
-//        return "/article/view";
-//    }
+        List<UsersDTO> users = usersService.findAll();
+        model.addAttribute(users);
+
+        // 회원 목록 불러오기
+        return "/admin/member/list";
+    }
 
     @GetMapping("/admin/member/point")
     public String point() {
--- lotteon/src/main/java/kr/co/lotteon/service/UsersService.java
+++ lotteon/src/main/java/kr/co/lotteon/service/UsersService.java
@@ -1,12 +1,15 @@
 package kr.co.lotteon.service;
 
+import kr.co.lotteon.dto.UsersDTO;
 import kr.co.lotteon.entity.Users;
 import kr.co.lotteon.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,9 +17,14 @@ import java.util.List;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final ModelMapper modelMapper;
 
-    public List<Users> findAll() {
-        return usersRepository.findAll();
+    public List<UsersDTO> findAll() {
+        List<Users> usersList = usersRepository.findAll();
+
+        return usersList.stream()
+                .map(users -> modelMapper.map(users, UsersDTO.class))
+                .collect(Collectors.toList());
     }
 
 }
