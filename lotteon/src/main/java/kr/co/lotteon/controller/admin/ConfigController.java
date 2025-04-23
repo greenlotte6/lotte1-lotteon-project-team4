744b2fc915702b2521e3b0ecaa2e1676485a59d1
gyeol00 <gyeol000726@gmail.com>
2025-04-23 17:13:14 +0900

    add all

--- lotteon/src/main/java/kr/co/lotteon/controller/admin/ConfigController.java
+++ lotteon/src/main/java/kr/co/lotteon/controller/admin/ConfigController.java
@@ -2,12 +2,16 @@ package kr.co.lotteon.controller.admin;
 
 import kr.co.lotteon.dto.TermsDTO;
 import kr.co.lotteon.entity.Terms;
+import kr.co.lotteon.repository.TermsRepository;
 import kr.co.lotteon.service.admin.ConfigService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,8 +44,19 @@ public class ConfigController {
         List<TermsDTO> termsDTOList = configService.findAll();
         model.addAttribute("terms", termsDTOList);
 
+        log.info("terms : " + termsDTOList);
+
         return "/admin/config/policy";
     }
+
+//    @PostMapping("/admin/config/policy")
+//    public String policy(Terms terms) {
+//        log.info("terms : " + terms);
+//        configService.modifyPolicy(terms);
+//
+//        return "redirect:/admin/config/policy";
+//    }
+
 
     @GetMapping("/admin/config/version")
     public String version() {
