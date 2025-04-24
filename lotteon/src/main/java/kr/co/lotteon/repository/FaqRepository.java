282e1f9454d20c7ebdc523153fc087b7db825a45
minsung <dpsdy1@gmail.com>
2025-04-24 17:09:35 +0900

    add

--- lotteon/src/main/java/kr/co/lotteon/repository/FaqRepository.java
+++ lotteon/src/main/java/kr/co/lotteon/repository/FaqRepository.java
@@ -1,7 +1,12 @@
 package kr.co.lotteon.repository;
 
 import kr.co.lotteon.entity.Faq;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FaqRepository extends JpaRepository<Faq, Integer> {
+    Page<Faq> findByType1(String type1, Pageable pageable);
+    Page<Faq> findByType1AndType2(String type1, String type2, Pageable pageable);
+
 }
