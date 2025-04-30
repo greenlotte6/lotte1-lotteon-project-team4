8530595874d0816b4fe5672b4543f7405a870461
minsung <dpsdy1@gmail.com>
2025-04-30 12:50:02 +0900

    add

---
+++ lotteon/src/main/java/kr/co/lotteon/service/admin/VersionService.java
@@ -0,0 +1,22 @@
+package kr.co.lotteon.service.admin;
+
+import kr.co.lotteon.entity.Coupon;
+import kr.co.lotteon.entity.Version;
+import kr.co.lotteon.repository.VersionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class VersionService {
+
+    private final VersionRepository versionRepository;
+
+    public List<Version> getAllVersions() {
+        return versionRepository.findAll();
+    }
+}
