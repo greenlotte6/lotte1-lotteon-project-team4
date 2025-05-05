d0fd27f07914da0fd2b75557a931877a6e1161ed
minsung <dpsdy1@gmail.com>
2025-05-05 21:30:19 +0900

    add

--- lotteon/src/main/java/kr/co/lotteon/repository/ProductRepository.java
+++ lotteon/src/main/java/kr/co/lotteon/repository/ProductRepository.java
@@ -2,10 +2,10 @@ package kr.co.lotteon.repository;
 
 import kr.co.lotteon.entity.Products;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Products, Integer> {
